336b298dbfa6beb425e26b83fb4082320b13e429
SomeBodyM <151923587+SomeBodyM@users.noreply.github.com>
2024-11-04 09:42:17 +0200

    add API_KEY, add ability to send requests

--- src/components/TaskBlock.tsx
+++ src/components/TaskBlock.tsx
@@ -4,96 +4,104 @@ import { TaskForm } from "./TaskForm";
 import { useCallback, useState, memo } from "react";
 import { ITask } from "../types/types";
 import { TaskItem } from "./TaksItem";
+import { addTask } from "../api/tasks";
 
 export const TaskBlock = memo(() => {
-  const addModal = useModal();
-  const editModal = useModal();
-  const [taskData, setTaskData] = useState<ITask[]>([]);
-  const [taskToEdit, setTaskToEdit] = useState<ITask>({});
+    const addModal = useModal();
+    const editModal = useModal();
+    const [taskData, setTaskData] = useState<ITask[]>([]);
+    const [taskToEdit, setTaskToEdit] = useState<ITask>({});
 
-  const onAddNewTask = useCallback(
-    (data: ITask) => {
-      const newTask = {
-        id: Date.now(),
-        title: data.title,
-        description: data.description,
-        status: false,
-        file: data.file
-      };
+    const onAddNewTask = useCallback(
+        async (data: ITask) => {
+            const newTask = {
+                id: Date.now(),
+                title: data.title,
+                description: data.description,
+                status: false,
+                file: data.file,
+            };
 
-      const updatedTasks = [...taskData, newTask];
-      setTaskData(updatedTasks);
-    },
-    [taskData]
-  );
+            const newTaskToServer = await addTask(data);
+            console.log(newTaskToServer);
 
-  const onEditTask = useCallback(
-    (editedTask: ITask) => {
-      const updatedTasksData = [
-        ...taskData.map((task) =>
-          task.id === editedTask.id ? editedTask : task
-        ),
-      ];
-      setTaskData(updatedTasksData);
-    },
-    [taskData]
-  );
+            const updatedTasks = [...taskData, newTask];
+            setTaskData(updatedTasks);
+        },
+        [taskData]
+    );
 
-  const onDeleteTask = useCallback(
-    (id: number) => {
-      const updatedTasks = taskData.filter((task) => task.id !== id);
-      setTaskData(updatedTasks);
-    },
-    [taskData]
-  );
+    const onEditTask = useCallback(
+        (editedTask: ITask) => {
+            const updatedTasksData = [
+                ...taskData.map((task) =>
+                    task.id === editedTask.id ? editedTask : task
+                ),
+            ];
+            setTaskData(updatedTasksData);
+        },
+        [taskData]
+    );
 
-  const onEditModalOpen = useCallback(
-    (task: ITask) => {
-      setTaskToEdit(task);
-      editModal.onToggleModal();
-    },
-    [editModal, setTaskToEdit]
-  );
+    const onDeleteTask = useCallback(
+        (id: number) => {
+            const updatedTasks = taskData.filter((task) => task.id !== id);
+            setTaskData(updatedTasks);
+        },
+        [taskData]
+    );
 
-  return (
-    <div className="w-full max-w-4xl">
-      {addModal.isOpen && (
-        <TaskForm
-          onClose={addModal.onToggleModal}
-          formTitle="Add Task"
-          successBtnText="Add Task"
-          callback={onAddNewTask}
-        />
-      )}
-      {editModal.isOpen && (
-        <TaskForm
-          data={taskToEdit}
-          onClose={editModal.onToggleModal}
-          formTitle="Edit Task"
-          successBtnText="Save"
-          callback={onEditTask}
-        />
-      )}
-      <div className="bg-neutral-50 p-4 border rounded-xl flex items-center justify-between">
-        <h1 className="text-violet-700 font-extrabold text-2xl">Task List</h1>
-        <Button onClick={addModal.onToggleModal}>Add</Button>
-      </div>
-      <div className="flex flex-col gap-2 mt-4">
-        {taskData.length > 0 ? (
-          taskData.map((item) => (
-            <TaskItem
-              key={item.id}
-              data={item}
-              onDelete={onDeleteTask}
-              onEdit={onEditModalOpen}
-            />
-          ))
-        ) : (
-          <div className="p-4 bg-neutral-50 border rounded-xl">
-            <h1 className="text-red-600 text-xl text-center">Empty List</h1>
-          </div>
-        )}
-      </div>
-    </div>
-  );
+    const onEditModalOpen = useCallback(
+        (task: ITask) => {
+            setTaskToEdit(task);
+            editModal.onToggleModal();
+        },
+        [editModal, setTaskToEdit]
+    );
+
+    return (
+        <div className="w-full max-w-4xl">
+            {addModal.isOpen && (
+                <TaskForm
+                    onClose={addModal.onToggleModal}
+                    formTitle="Add Task"
+                    successBtnText="Add Task"
+                    callback={onAddNewTask}
+                />
+            )}
+            {editModal.isOpen && (
+                <TaskForm
+                    data={taskToEdit}
+                    onClose={editModal.onToggleModal}
+                    formTitle="Edit Task"
+                    successBtnText="Save"
+                    callback={onEditTask}
+                />
+            )}
+            <div className="bg-neutral-50 p-4 border rounded-xl flex items-center justify-between">
+                <h1 className="text-violet-700 font-extrabold text-2xl">
+                    Task List
+                </h1>
+                <Button onClick={addModal.onToggleModal}>Add</Button>
+            </div>
+            <div className="flex flex-col gap-2 mt-4">
+                {taskData.length > 0 ? (
+                    taskData.map((item) => (
+                        <TaskItem
+                            key={item.id}
+                            data={item}
+                            onDelete={onDeleteTask}
+                            onEdit={onEditModalOpen}
+                        />
+                    ))
+                ) : (
+                    <div className="p-4 bg-neutral-50 border rounded-xl">
+                        <h1 className="text-red-600 text-xl text-center">
+                            Empty List
+                        </h1>
+                    </div>
+                )}
+            </div>
+        </div>
+    );
 });
--- src/components/TaskForm.tsx
+++ src/components/TaskForm.tsx
@@ -5,91 +5,102 @@ import { Modal } from "../ui/Modal";
 import { ITask } from "../types/types";
 
 interface TaskFormProps {
-  data?: ITask;
-  onClose: () => void;
-  formTitle: string;
-  callback: (data: ITask) => void;
-  successBtnText: string;
+    data?: ITask;
+    onClose: () => void;
+    formTitle: string;
+    callback: (data: ITask) => void;
+    successBtnText: string;
 }
 
 export const TaskForm = memo((props: TaskFormProps) => {
-  const { data, onClose, formTitle, callback, successBtnText } = props;
+    const { data, onClose, formTitle, callback, successBtnText } = props;
 
-  const [title, setTitle] = useState(data?.title || "");
-  const [description, setDescription] = useState(data?.description || "");
-  const [status, setStatus] = useState(data?.status || false);
-  const [file, setFile] = useState<File | null>(data?.file || null);
-  const [fileName, setFileName] = useState(data?.file?.name || '');
+    const [title, setTitle] = useState(data?.title || "");
+    const [description, setDescription] = useState(data?.description || "");
+    const [status, setStatus] = useState(data?.status || false);
+    const [file, setFile] = useState<File | null>(data?.file || null);
+    const [fileName, setFileName] = useState(data?.file?.name || "");
 
-  const onTitleHandler = useCallback((value: string) => {
-    setTitle(value);
-  }, []);
+    const onTitleHandler = useCallback((value: string) => {
+        setTitle(value);
+    }, []);
 
-  const onDescriptionHandler = useCallback((value: string) => {
-    setDescription(value);
-  }, []);
+    const onDescriptionHandler = useCallback((value: string) => {
+        setDescription(value);
+    }, []);
 
-  const onStatusHandler = useCallback(() => {
-    setStatus(!status);
-  }, [status]);
+    const onStatusHandler = useCallback(() => {
+        setStatus(!status);
+    }, [status]);
 
-  const onFileHandler = useCallback(
-    (e: React.ChangeEvent<HTMLInputElement>) => {
-      if (e.target.files) {
-        const file = e.target.files[0];
-        setFileName(file.name)
-        setFile(file);
-      }
-    },
-    []
-  );
+    const onFileHandler = useCallback(
+        (e: React.ChangeEvent<HTMLInputElement>) => {
+            if (e.target.files) {
+                const file = e.target.files[0];
+                setFileName(file.name);
+                setFile(file);
+            }
+        },
+        []
+    );
 
-  const onSubmit = useCallback((e: React.FormEvent) => {
-    e.preventDefault();
-  }, []);
+    const onSubmit = useCallback((e: React.FormEvent) => {
+        e.preventDefault();
+    }, []);
 
-  const onFormSubmit = useCallback(() => {
-    const formData = { id: data?.id, title, description, status, file };
-    callback(formData);
+    const onFormSubmit = useCallback(() => {
+        if (!title) {
+            return alert("task title is required");
+        }
+        
+        const formData = { id: data?.id, title, description, status, file };
+        callback(formData);
 
-    setTitle("");
-    setDescription("");
-    onClose();
-  }, [callback, title, description, status, data?.id, onClose, file]);
+        setTitle("");
+        setDescription("");
+        onClose();
+    }, [callback, title, description, status, data?.id, onClose, file]);
 
-  return (
-    <Modal>
-      <form className="flex flex-col gap-4" onSubmit={onSubmit}>
-        <h1 className="text-violet-700 font-extrabold text-2xl">{formTitle}</h1>
-        <Input
-          onChange={onTitleHandler}
-          value={title}
-          placeholder="Enter Title"
-        />
-        <Input
-          onChange={onDescriptionHandler}
-          value={description}
-          placeholder="Enter Description"
-        />
-        <div className="flex items-center justify-between">
-        <input type="file" onChange={onFileHandler}/>
-        <label className="text-ellipsis text-red-600" htmlFor="file">{fileName}</label>
-        </div>
-        {data && (
-          <Button
-            onClick={onStatusHandler}
-            bgColor={status ? "bg-green-600" : "bg-red-600"}
-          >
-            {status ? "Completed" : "Mark as Completed"}
-          </Button>
-        )}
-        <div className="w-full flex items-center justify-between">
-          <Button onClick={onClose} bgColor="bg-red-700">
-            Close
-          </Button>
-          <Button onClick={onFormSubmit}>{successBtnText}</Button>
-        </div>
-      </form>
-    </Modal>
-  );
+    return (
+        <Modal>
+            <form className="flex flex-col gap-4" onSubmit={onSubmit}>
+                <h1 className="text-violet-700 font-extrabold text-2xl">
+                    {formTitle}
+                </h1>
+                <Input
+                    onChange={onTitleHandler}
+                    value={title}
+                    placeholder="Enter Title"
+                />
+                <Input
+                    onChange={onDescriptionHandler}
+                    value={description}
+                    placeholder="Enter Description"
+                />
+                <div className="flex items-center justify-between">
+                    <input type="file" onChange={onFileHandler} />
+                    <label
+                        className="text-ellipsis text-red-600"
+                        htmlFor="file"
+                    >
+                        {fileName}
+                    </label>
+                </div>
+                {data && (
+                    <Button
+                        onClick={onStatusHandler}
+                        bgColor={status ? "bg-green-600" : "bg-red-600"}
+                    >
+                        {status ? "Completed" : "Mark as Completed"}
+                    </Button>
+                )}
+                <div className="w-full flex items-center justify-between">
+                    <Button onClick={onClose} bgColor="bg-red-700">
+                        Close
+                    </Button>
+                    <Button onClick={onFormSubmit}>{successBtnText}</Button>
+                </div>
+            </form>
+        </Modal>
+    );
 });
